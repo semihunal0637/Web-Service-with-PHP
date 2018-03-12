4267d609abf7d2595bb31de4466891ff3ccf694b
semihunal0637 <semih1837@gmail.com>
2018-03-12 21:31:53 +0300

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 
 
-$conn = pg_connect("host=localhost port=5432 dbname=bilgiler user=postgres password='06370637'");
+$conn = pg_connect("host=localhost port=5432 dbname=bilgiler user=postgres password='PASSWORD'");
 
 if($conn == true ) {
             //echo "<h2><font color='green'>Bağlantı Başarıyla Yapıldı!</font></h2></br>";
